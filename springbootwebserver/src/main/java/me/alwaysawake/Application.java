236de9de61cc97c45803c698138443b88b387699
leemirim <leemirim9654@gmail.com>
2019-07-10 20:19:06 +0900

    수정

--- springbootwebserver/src/main/java/me/alwaysawake/Application.java
+++ springbootwebserver/src/main/java/me/alwaysawake/Application.java
@@ -1,42 +1,13 @@
 package me.alwaysawake;
 
-import org.apache.catalina.Context;
-import org.apache.catalina.LifecycleException;
-import org.apache.catalina.startup.Tomcat;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-import java.io.PrintWriter;
-
+@SpringBootApplication
 public class Application {
 
-    public static void main(String[] args) throws LifecycleException {
-        Tomcat tomcat = new Tomcat();
-        tomcat.setPort(8080);
-
-        Context context = tomcat.addContext("/", "/");
-
-        HttpServlet httpServlet = new HttpServlet() {
-            @Override
-            protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-                PrintWriter printWriter = resp.getWriter();
-                printWriter.println("<html><head><title>");
-                printWriter.println("Hey, Tomcat");
-                printWriter.println("</title></head>");
-                printWriter.println("<body><h1>Hello Timcat</h1></body>");
-                printWriter.println("</html>");
-
-            }
-        };
-        String servletName = "helloServlet";
-        tomcat.addServlet("/", servletName, httpServlet);
-        context.addServletMappingDecoded("/hello", servletName);
-
-        tomcat.start();
-//        tomcat.getServer().await();
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class,args);
     }
 
 }
